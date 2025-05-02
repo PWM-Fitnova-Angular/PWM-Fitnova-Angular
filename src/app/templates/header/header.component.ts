97276a20a96ee84a0cc2a0ebfae3efe0744f092a
Joel Santana Aleman <26joelaleman@gmail.com>
2025-05-02 12:44:42 +0100

    login and logout

--- src/app/templates/header/header.component.ts
+++ src/app/templates/header/header.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { auth } from '../../../firebase/firebase_config';
+import {NgIf} from '@angular/common';
+
 
 @Component({
   selector: 'app-header',
-    imports: [
-        RouterLink
-    ],
+  imports: [
+    RouterLink,
+    NgIf
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isLoggedIn = false;
+
+  ngOnInit() {
+    onAuthStateChanged(auth, user => {
+      this.isLoggedIn = !!user;
+    });
+  }
+  logout() {
+    signOut(auth)
+      .then(() => console.log('Sesión cerrada'))
+      .catch(err => console.error('Error al cerrar sesión', err));
+  }
 
 }

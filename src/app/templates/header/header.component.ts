5a748b992c2de8b8f15a096dec92f795189def4e
Raul <raultrejito53@gmail.com>
2025-05-03 19:05:32 +0100

    wip:header, footer

--- src/app/templates/header/header.component.ts
+++ src/app/templates/header/header.component.ts
@@ -1,9 +1,9 @@
-import {RouterLink} from "@angular/router";
+import { RouterLink, Router, NavigationEnd } from "@angular/router";
 import { Component, OnInit } from '@angular/core';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { auth } from '../../../firebase/firebase_config';
-import {NgIf} from '@angular/common';
-
+import { NgIf } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -14,18 +14,39 @@ import {NgIf} from '@angular/common';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   isLoggedIn = false;
+  isAuthPage = false;
+
+  constructor(private router: Router) {}
 
   ngOnInit() {
+    // Verificar autenticación con Firebase
     onAuthStateChanged(auth, user => {
       this.isLoggedIn = !!user;
     });
-  }
-  logout() {
-    signOut(auth)
-      .then(() => console.log('Sesión cerrada'))
-      .catch(err => console.error('Error al cerrar sesión', err));
+
+    // Verificar la ruta actual al iniciar
+    this.checkIfAuthPage(this.router.url);
+
+    // Suscribirse a cambios de ruta
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.checkIfAuthPage(event.url);
+    });
   }
 
+  private checkIfAuthPage(url: string): void {
+    this.isAuthPage = url === '/register' || url === '/' || url === '/login';
+  }
+
+  logout() {
+    signOut(auth)
+      .then(() => {
+        console.log('Sesión cerrada');
+        this.router.navigate(['/login']);
+      })
+      .catch(err => console.error('Error al cerrar sesión', err));
+  }
 }

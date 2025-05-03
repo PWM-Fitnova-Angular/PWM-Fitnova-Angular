ee28333dc861ebcea01995a25ba64f49d50ae1f0
Joel Santana Aleman <26joelaleman@gmail.com>
2025-05-03 13:13:48 +0100

    mockups, pictures

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
+import {onAuthStateChanged, signOut} from 'firebase/auth';
+import {auth} from '../../../firebase/firebase_config';
 
 @Component({
   selector: 'app-profile',
@@ -10,5 +12,21 @@ import {RouterLink} from '@angular/router';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  isLoggedIn = false;
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    onAuthStateChanged(auth, user => {
+      this.isLoggedIn = !!user;
+    });
+  }
+  logout(): void {
+    signOut(auth)
+      .then(() => {
+        console.log('Sesión cerrada');
+        this.router.navigate(['/login']);
+      })
+      .catch(err => console.error('Error al cerrar sesión', err));
+  }
 }
